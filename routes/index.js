f58bd93493215a6a27879d237ca8da50331f3f16
ahmed-mohadin <ahmed.mohadin@outlook.com>
2022-09-13 14:51:28 +0200

    Added style and clean code

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var messages = require('../public/javascripts/messages');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    info: 'Welcome to Mini Messageboard',
+    messages: messages,
+  });
 });
 
 module.exports = router;
